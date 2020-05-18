84b511079aee713d4bdc0938e2dfa7f18233a076
Me <pc@Mes-MacBook-Pro.local>
2020-05-18 21:07:55 +0400

    added product data structure

---
+++ interfaces/IProductAttributes.ts
@@ -0,0 +1,22 @@
+export interface IProductPhoto {
+    url: string;
+    alt?: string;
+    isMain: Boolean;
+}
+
+export interface IProductAttributes {
+    id?: string;
+    photos: IProductPhoto[];
+    title: string;
+    year?: number;
+    author: string;
+    price: Number;
+    type?: string;
+    pages?: Number;
+    stockAmount?: Number;
+    description?: string;
+    category: [string];
+    rating?: Number;
+    isFeaturedOnMain?: Boolean;
+    currency?: string;
+}
