74e10ed7b571aca07834bbfbd40671b08aca91d5
Me <pc@Mes-MacBook-Pro.local>
2020-05-20 18:49:32 +0400

    Made logo clickable

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { FaShoppingBasket } from 'react-icons/fa';
+import Link from 'next/link';
 
 export default function Header() {
   const count: number = 0;
 
   return (
     <header>
-      <img width="95px" src="/logo.png" alt="Site Logo" />
+      <Link href="/">
+        <img width="95px" src="/logo.png" alt="Site Logo" />
+      </Link>
       <div>
         <button type="button">Login</button>
         <button type="button">Register</button>
