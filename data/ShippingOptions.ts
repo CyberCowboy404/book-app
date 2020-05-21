d1c2e23014520fbc947f51ec1315d59bddaf64ad
Me <pc@Mes-MacBook-Pro.local>
2020-05-21 20:15:17 +0400

    Create data mock and interface for shipping information

---
+++ data/ShippingOptions.ts
@@ -0,0 +1,16 @@
+const shippingOptions = [{
+  name: 'Regular delivery',
+  price: 5,
+  currency: '$',
+  description: 'In 5-7 business days.',
+  id: 123,
+},
+{
+  name: 'Express delivery',
+  price: 15,
+  currency: '$',
+  description: 'In 2-4 business days.',
+  id: 124,
+}];
+
+export default shippingOptions;
---
+++ interfaces/IShippingInformation.ts
@@ -0,0 +1,9 @@
+/* eslint-disable semi */
+/* eslint-disable no-extra-semi */
+export default interface IShippingInformation {
+  name: string,
+  price: number,
+  currency: string,
+  description: string,
+  id: number,
+};
