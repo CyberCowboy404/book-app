cf09d411a628f1a63c206f20de9d92a6eeecf1a2
Me <pc@Mes-MacBook-Pro.local>
2020-05-22 17:54:32 +0400

    added structure for delivery info and payment info

--- pages/checkout/order.tsx
+++ pages/checkout/order.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { Component } from 'react';
 import Layout from '../../components/common/Layout';
 import Login from '../../components/auth/Login';
@@ -67,6 +68,62 @@ export default class Order extends Component<{}, OrderState> {
           </div>
         </div>
         {content}
+        <section>
+          <h3>Delivery details</h3>
+          <div>
+            <div>
+              <label htmlFor="firstName">First name:</label>
+              <input name="firstName" type="text" id="firstName" />
+            </div>
+            <div>
+              <label htmlFor="lastName">Last name:</label>
+              <input name="lastName" type="text" id="lastName" />
+            </div>
+            <div>
+              <label htmlFor="firstName">Country:</label>
+              <select name="country" id="country">
+                <option value="usa">United States</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="address">Address:</label>
+              <input name="address" type="text" id="address" />
+            </div>
+            <div>
+              <label htmlFor="state">State:</label>
+              <select name="state" id="state">
+                <option value="AL">Alabama</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="city">City:</label>
+              <input name="city" type="text" id="city" />
+            </div>
+          </div>
+        </section>
+        <section>
+          <h3>Payment details</h3>
+          <div>
+            <label htmlFor="fullName">Name on card:</label>
+            <input name="fullName" type="text" id="fullName" />
+          </div>
+          <div>
+            <label htmlFor="cardNumber">Card number:</label>
+            <input name="cardNumber" type="text" id="cardNumber" />
+          </div>
+          <div>
+            <span>Expire date: </span>
+            <select name="month" id="month">
+              <option value="">Month</option>
+            </select>
+            <select name="year" id="year">
+              <option value="">Year</option>
+            </select>
+          </div>
+        </section>
+        <div>
+          <button type="button">Pay</button>
+        </div>
       </Layout>
     );
   }
